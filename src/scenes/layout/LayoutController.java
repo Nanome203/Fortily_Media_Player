6823ed1efb1f30cd78ebb76bbdc73a699df7d60c
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-05-09 22:46:52 +0700

    add splash screen

---
+++ src/scenes/layout/LayoutController.java
@@ -0,0 +1,16 @@
+package scenes.layout;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class LayoutController {
+
+    @FXML
+    private BorderPane mainContainer;
+
+    @FXML
+    private VBox sidebarNavigator;
+    
+
+}
