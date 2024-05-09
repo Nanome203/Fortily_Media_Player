4bc97150c1b4f0812d94a104dfcd5cb08b56ae49
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-05-09 15:51:24 +0700

    add app icon and move assets folder to src folder

--- src/application/EntryPoint.java
+++ src/application/EntryPoint.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class EntryPoint extends Application{
 	  @Override
 	    public void start(Stage primaryStage) throws Exception{
 	        Parent root = FXMLLoader.load(getClass().getResource("/scenes/mainScene/TestScene.fxml"));
+	        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/images/Fortily.png")));
 	        primaryStage.setTitle("Fortily");
 	        primaryStage.setScene(new Scene(root));
 	        primaryStage.show();
