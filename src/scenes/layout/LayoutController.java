d013fd9ce2a4c19fde1dab34c2243ecf6a0b9b32
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-05-13 13:00:50 +0700

    update layout

--- src/scenes/layout/LayoutController.java
+++ src/scenes/layout/LayoutController.java
@@ -1,16 +1,50 @@
 package scenes.layout;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class LayoutController {
 
-    @FXML
-    private BorderPane mainContainer;
+	  @FXML
+	    private HBox imageSongContainer;
 
-    @FXML
-    private VBox sidebarNavigator;
-    
+	    @FXML
+	    private BorderPane mainContainer;
+
+	    @FXML
+	    private Button sideBarFav;
+
+	    @FXML
+	    private Button sideBarHome;
+
+	    @FXML
+	    private Button sideBarPlaylist;
+
+	    @FXML
+	    private VBox sidebarNavigator;
+
+	    @FXML
+	    private Button siderBarMusicLib;
+
+	    @FXML
+	    private Button siderBarVideoLib;
+	    
+	    @FXML
+	    private VBox sideBarContainer;
+
+		
+	    public void selectItem(ActionEvent event) {
+	        for (Node node : sidebarNavigator.getChildren()) {
+	                ((Button)node).getStyleClass().remove("active");
+	        }
+
+	        // Add 'selected' class to the clicked item
+	        ((Button)event.getSource()).getStyleClass().add("active");
+	    }
 
 }
