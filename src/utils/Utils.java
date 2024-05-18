dd28bd737b4e30fd1d4a1d11615577b4e84e7e97
jackesque <127185476+Jackesque@users.noreply.github.com>
2024-05-18 23:11:16 +0700

    fix utils formattime

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -5,12 +5,9 @@ import javafx.util.Duration;
 public class Utils {
   public static String formatTime(Duration duration) {
     int intDuration = (int) Math.floor(duration.toSeconds());
-    int hours = intDuration / (60 * 60);
-    if (hours > 0) {
-      intDuration -= hours * 60 * 60;
-    }
-    int minutes = intDuration / 60;
-    int seconds = intDuration - hours * 60 * 60 - minutes * 60;
+    int hours = intDuration / 3600;
+    int minutes = (intDuration % 3600) / 60;
+    int seconds = intDuration % 60;
 
     if (hours > 0) {
       return String.format("%d:%02d:%02d", hours, minutes, seconds);
