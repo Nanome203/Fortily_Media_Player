a6f04c3c26f14b619843931dd607d34d61c1f9dc
jackesque <127185476+Jackesque@users.noreply.github.com>
2024-06-10 22:31:35 +0700

    add about controller

---
+++ src/scenes/about/AboutController.java
@@ -0,0 +1,35 @@
+package scenes.about;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+public class AboutController implements Initializable {
+  @FXML
+  private BorderPane mainAboutContainer;
+
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("You Trial Has Ended");
+    alert.setHeaderText(null);
+    alert.setContentText("Your free trial has ended. Please pay 23.000VND for full access to WinRAR.");
+
+    ButtonType payButton = new ButtonType("Pay");
+    ButtonType cancelButton = new ButtonType("I'll use 7-Zip instead");
+    alert.getButtonTypes().setAll(payButton, cancelButton);
+    alert.showAndWait().ifPresent(buttonType -> {
+      if (buttonType == payButton) {
+        return;
+      } else if (buttonType == cancelButton) {
+        return;
+      }
+    });
+    mainAboutContainer.setCenter(alert.getDialogPane());
+  }
+}
